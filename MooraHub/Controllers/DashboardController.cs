3fd372135fa89aebe19758e1ddbad08113f04314
thabisoelseventyseven-del <thabisoelseventyseven@gmail.com>
2026-01-14 09:13:43 +0200

    Added floating hero logo with tech glow to landing page

---
+++ MooraHub/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MooraHub.Controllers
+{
+    [Authorize] // 🔒 Only logged-in users can access
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
